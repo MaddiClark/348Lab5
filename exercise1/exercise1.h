22a301722ca1c009879053cb7596d37714634320
MaddiClark <cclar@ku.edu>
2026-03-08 20:40:05 -0500

    Update exercise1.h

--- exercise1/exercise1.h
+++ exercise1/exercise1.h
@@ -1,9 +1,9 @@
-#ifndef MY_HEADER_H
-# define MY_HEADER_H
-void monthlyReport(float, const char *);
-void salesSummary(float, const char *);
-void movingAverage(float, const char *);
-void merge(float, const char *, int, int, int);
-void mergeSort(float, const char *, int, int);
-void highestToLowest(float, const char *);
+#ifndef LAB05_H
+# define LAB05_H
+void monthlyReport(float input[], const char *months[]);
+void salesSummary(float input[], const char *months[]);
+void movingAverage(float input[], const char *months[]);
+void merge(float input[], const char *months[], int left, int mid, int right);
+void mergeSort(float input[], const char *months[], int left, int right);
+void highestToLowest(float input[], const char *months[]);
 #endif
